847f0cbf7c9bf9164950fe2fa7c04a53165c736d
Austin Ek <mraustinek@gmail.com>
2018-02-09 18:48:50 -0600

    added config files

---
+++ src/main/config.kt
@@ -0,0 +1,38 @@
+package main
+
+val configString = "{\n" +
+        "  \"robots\": [\n" +
+        "    {\n" +
+        "      \"name\": \"Surus\",\n" +
+        "      \"width\": 1,\n" +
+        "      \"height\": 1\n" +
+        "    },\n" +
+        "    {\n" +
+        "      \"name\": \"Hushpuppy\",\n" +
+        "      \"width\": 1,\n" +
+        "      \"height\": 1\n" +
+        "    }\n" +
+        "  ],\n" +
+        "  \"levels\":[\n" +
+        "    {\n" +
+        "      \"properties\": {\n" +
+        "        \"name\": \"My Level\",\n" +
+        "        \"difficulty\": 0,\n" +
+        "        \"height\": 10,\n" +
+        "        \"width\": 10\n" +
+        "      },\n" +
+        "      \"robot-Players\": [\n" +
+        "        {\n" +
+        "          \"name\": \"Surus\",\n" +
+        "          \"startX\": 0,\n" +
+        "          \"startY\": 0\n" +
+        "        },\n" +
+        "        {\n" +
+        "          \"name\": \"Hushpuppy\",\n" +
+        "          \"startX\": 1,\n" +
+        "          \"startY\": 1\n" +
+        "        }\n" +
+        "      ]\n" +
+        "    }\n" +
+        "  ]\n" +
+        "}"
