a27d36aa67e54768cc690ec0015da451b19b275d
Austin Ek <mraustinek@gmail.com>
2018-03-13 16:40:08 -0500

    removed confusing availableInstructions list

--- backend/src/org/chicagoedt/rosette/Instructions/Instruction.kt
+++ backend/src/org/chicagoedt/rosette/Instructions/Instruction.kt
@@ -3,13 +3,6 @@ package org.chicagoedt.rosette.Instructions
 import org.chicagoedt.rosette.Levels.Level
 import org.chicagoedt.rosette.Robots.RobotPlayer
 
-val availableInstructions = arrayListOf<Pair<String, String>>(
-        Pair("Move", "Int"),
-        Pair("Turn", "Rotation"),
-        Pair("Conditional", "Comparison"),
-        Pair("Read Sensor", "Sensor")
-        )
-
 abstract class Instruction {
     abstract var parameter : Any
 
