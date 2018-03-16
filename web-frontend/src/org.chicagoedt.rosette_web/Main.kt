b43586a4e3a9bec25ded804a4b0ad8e4efe06206
Austin Ek <mraustinek@gmail.com>
2018-03-16 17:13:36 -0500

    configured kotlin to js compilation

---
+++ web-frontend/src/org.chicagoedt.rosette_web/Main.kt
@@ -0,0 +1,9 @@
+package org.chicagoedt.rosette_web
+
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
+import kotlin.browser.*
+
+fun main(args: Array<String>) {
+    console.log("Hello world!")
+}
