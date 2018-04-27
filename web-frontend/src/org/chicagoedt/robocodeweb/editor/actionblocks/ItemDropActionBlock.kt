bf240fc4b75eb8442bcebe37cf155a6b3766c440
Austin Ek <mraustinek@gmail.com>
2018-04-27 13:28:03 -0500

    drop item block

---
+++ web-frontend/src/org/chicagoedt/robocodeweb/editor/actionblocks/ItemDropActionBlock.kt
@@ -0,0 +1,15 @@
+package org.chicagoedt.robocodeweb.editor.actionblocks
+
+import org.chicagoedt.robocode.actions.robotActions.ItemDropAction
+import org.chicagoedt.robocode.actions.robotActions.ItemPickupAction
+import org.chicagoedt.robocode.collectibles.etc.Sand
+import org.chicagoedt.robocodeweb.editor.ActionBlock
+
+class ItemDropActionBlock : ActionBlock<ItemDropAction>() {
+    override val action =  ItemDropAction()
+    override val hasParameters = true
+
+    init{
+        insertParameter("Sand", Sand.id)
+    }
+}
